64d86e03bc34d25d1d71a2f2afa42244ae329811
Ayush Shukla <ayushshukla.personal@gmail.com>
2026-07-06 16:38:46 +0530

    added gitignore

--- build/drive_controls/CMakeFiles/drive_controls_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/drive_controls_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/drive_controls_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/kratos_controllers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ayush/ARC_26/src/kratos_controllers
-# Build directory: /home/ayush/ARC_26/build/kratos_controllers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
